5a0330b0b72136f691b1bd0de7b42baee9c78389
Martin-Luther Obahor <kingm4159@gmail.com>
2023-07-13 18:49:54 +0100

    added featuresProjects section

---
+++ src/content/index.ts
@@ -0,0 +1,10 @@
+export const rollWithSection = {
+	header: 'Roll with the professionals',
+	paragraph: 'Lorem ipsum dolor sit amet consectetur. Aliquam amet tempor habitant est maecenas ornare neque?',
+	cta: 'Contact us'
+}
+
+export const featuredProjects = {
+	header: 'Featured products',
+	cta: 'view all products'
+}
