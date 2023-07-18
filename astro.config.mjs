f4f3f3a44a2896513d98a1d72ed7d5521fd85085
Martin-Luther Obahor <kingm4159@gmail.com>
2023-07-18 23:58:11 +0100

    our process section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import {defineConfig} from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import react from '@astrojs/react';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
 
-import image from "@astrojs/image";
+import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), react(), image()]
-});
+	integrations: [
+		tailwind(),
+		react(),
+		image()
+	]
+})
