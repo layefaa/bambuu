76809b381254a56deb1ee6e2a65b7c8f6d5964fd
Martin-Luther Obahor <kingm4159@gmail.com>
2023-07-28 03:22:04 +0100

    added hero section

--- src/content/index.ts
+++ src/content/index.ts
@@ -13,3 +13,8 @@ export const ourProcess = {
 	header: 'Our Process In Making Furniture',
 	paragraph: 'These are some of our processes in making furniture, of course with very experienced workers in their fields so that it can be a perfect product.'
 }
+
+export const Hero = {
+	paragraph: 'Lorem ipsum dolor sit amet consectetur. Aliquam amet tempor habitant est maecenas ornare neque?',
+	cta : 'Contact us'
+}
