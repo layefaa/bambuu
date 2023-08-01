24ae539e2fd1c8a13692b4132c0b8530c417239e
Martin-Luther Obahor <kingm4159@gmail.com>
2023-08-01 23:54:34 +0100

    ui: links

--- src/components/atoms/Link.tsx
+++ src/components/atoms/Link.tsx
@@ -4,8 +4,9 @@ import type { link } from '@interface/*'
 
 const Link = ({ name, url, classes }: link) => {
 	return (
-		<a href={url} className={`font-[400] ${classes}`}>
+		<a href={url} className={`font-[400] flex flex-col ${classes}`}>
 			{name}
+			<span style={{transitionProperty: 'width'}} className='transition ease-in h-[1px] bg-black opacity-0'/>
 		</a>
 	)
 }
