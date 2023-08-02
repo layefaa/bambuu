4604f1fe1e76fedf20a8b905d1b8620f597c2b4f
Martin-Luther Obahor <kingm4159@gmail.com>
2023-08-02 23:32:55 +0100

    ui:stat section

--- src/content/index.ts
+++ src/content/index.ts
@@ -16,5 +16,24 @@ export const ourProcess = {
 
 export const Hero = {
 	paragraph: 'Lorem ipsum dolor sit amet consectetur. Aliquam amet tempor habitant est maecenas ornare neque?',
-	cta : 'Contact us'
+	cta: 'Contact us'
+}
+
+export const Stat = {
+	header: 'Lorem ipsum',
+	paragraph: 'Lorem ipsum dolor sit amet consectetur. Suspendisse nisl turpis in maecenas in suscipit. Tincidunt ultricies quis bibendum ultricies in non lectus cras at. Condimentum ullamcorper dignissim vulputate risus convallis ullamcorper vel id. Suspendisse nisl turpis in maecenas in suscipit. Tincidunt ultricies quis bibendum ultricies in non lectus cras at.',
+	infos: [
+		{
+			number: 750,
+			category: 'Furniture or  home equipment'
+		},
+		{
+			number: 150,
+			category: 'Fresh interior design theme'
+		},
+		{
+			number: 850,
+			category: 'Happy client more for this'
+		}
+	]
 }
