d783e340e451d9624df912504d5d325d9288132f
Martin-Luther Obahor <layefa@Martin-Luthers-MacBook-Pro.local>
2023-09-12 18:07:07 +0100

    ux : menu switching

--- src/components/atoms/Link.tsx
+++ src/components/atoms/Link.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
-import type {link} from '@interface/*'
+import type { link } from '@interface/*'
 
 
-const Link = ({name, url, classes, animate}: link) => {
-    return (
-        <a href={url} className={`font-[400] flex flex-col group ${classes}`}>
-            {name}
-            {
-                animate
-                    ? <span style={{transitionProperty: 'width'}}
-                            className={`transition group-hover:w-[80%] ease-in h-[1px] bg-black w-0`}/>
-                    : null
-            }
+const Link = ({ name, url, classes, animate }: link) => {
+	return (
+		<a href={url} className={`font-[400] flex flex-col group ${classes}`}>
+			{name}
+			{
+				animate
+					? <span style={{ transitionProperty: 'width' }}
+									className={`transition group-hover:w-[80%] ease-in h-[1px] bg-black w-0`} />
+					: null
+			}
 
-        </a>
-    )
+		</a>
+	)
 }
 
 export default Link
