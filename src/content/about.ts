337c85e33787456e6871d5618d4651a160762f4d
Martin-Luther Obahor <kingm4159@gmail.com>
2023-10-03 23:08:52 +0100

    added contents

--- src/content/about.ts
+++ src/content/about.ts
@@ -14,3 +14,9 @@ export const ourValues = {
 	header: 'Our Values',
 	paragraph: 'Lorem ipsum dolor sit amet consectetur. Libero scelerisque id aliquam tristique magna tortor. Ultricies dictum cursus pretium lorem duis duis. Nunc eget in mauris dui. A etiam nulla integer elementum. Porttitor erat nibh non eget mauris at ultrices non mauris. Habitant non congue elementum interdum varius sed sit. Lectus suscipit egestas ullamcorper lobortis ornare ac. Sagittis diam diam sed ipsum leo euismod enim sed luctus.'
 }
+
+export const team = {
+	header: 'Meet the Team'
+}
+
+export const quote = 'Lorem ipsum dolor sit amet consectetur. Aliquam amet tempor habitant est maecenas ornare neque?'
