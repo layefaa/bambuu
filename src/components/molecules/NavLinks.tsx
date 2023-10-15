e4f466f0382fe5e6f5551aad16b59058a0665a5c
Martin-Luther Obahor <kingm4159@gmail.com>
2023-10-15 22:37:37 +0100

    sematic html for seo

--- src/components/molecules/NavLinks.tsx
+++ src/components/molecules/NavLinks.tsx
@@ -1,32 +1,6 @@
 import { links } from '@content/info'
 import Link from '@atoms/Link'
 
-// ---
-// import Link from '@atoms/Link'
-
-// interface Props {
-// 	classes: string
-// }
-//
-// const { classes } = Astro.props
-// ---
-//
-// 	)
-// 	}
-
-
-// <script>
-// 	const nav = document.querySelector('.navbar')
-// 	const navLinks = nav.querySelectorAll('a')
-// 	const currentURL = window.location.href
-// 	navLinks.forEach((link) => {
-// 	if (link.href === currentURL) {
-// 	link.classList.add('activeLink')
-// 	link.children[0].classList.add('activeLine')
-// }
-// })
-// </script>
-
 
 const NavLinks = () => {
 	return (
@@ -34,7 +8,9 @@ const NavLinks = () => {
 			{
 				links.map((x, index) => {
 						return (
-							<Link  key={index} url={x.url} name={x.name} animate={true} />
+							<li className={'list-none'} key={index}>
+								<Link url={x.url} name={x.name} animate={true} />
+							</li>
 						)
 					}
 				)
